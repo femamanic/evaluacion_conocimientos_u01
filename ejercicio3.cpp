a940ccd17f29af7f8d78e2c9119f688ea6986f48
femamanic <femamanic@unjbg.edu.pe>
2024-06-24 05:00:00 -0500

    Agregado a ejercicio3.cpp : el número debe ser mayor a 0 y menú

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -7,9 +7,33 @@ int factorial (int a);
 int main () {
     int n;
     float sum;
-    cout << "Introduce un numero: "; cin >> n;
-    sum = suma(n);
-    cout << sum << endl;
+    char re;
+    do {
+        system ("cls");
+        cout << "===============================================\n";
+        cout << "\t\t    SUMA DE\n";
+        cout << "1!/(2*1) + 2!/(2*2) + 3!/(2*3) + ... + n!/(2*n)\n";
+        cout << "===============================================\n";
+        cout << "Introduce un numero: "; cin >> n;
+        if (n <= 0){
+            cout << "===============================================\n";
+            cout << "El numero debe ser mayor a cero\n";
+
+        }
+        else {
+        sum = suma(n);
+        cout << "===============================================\n";
+        cout << "La suma total es: " << sum << endl;
+        }
+        cout << "===============================================\n";
+        cout << "Desea realizar otra operacion? (s/n): "; cin >> re;
+    } 
+    while (re == 's' || re == 'S');
+    if (re == 'n' || re == 'N'){
+        cout << "===============================================\n";
+        cout << "Saliendo del programa ...\n";
+        system ("pause>nul");
+    }
     return 0;
 }
 
