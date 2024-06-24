a01650f60017e19fb19efd2f8e00e81059424986
femamanic <femamanic@unjbg.edu.pe>
2024-06-24 04:11:43 -0500

    Agregado a ejercicio2.cpp : los números deben ser mayor a 0 y menú

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,20 +1,41 @@
 #include <iostream>
 using namespace std;
 int main () {
-    int n1, n2, c = 0, i = 1;
-    cout << "Introduce el primer numero: "; cin >> n1;
-    cout << "Introduce el segundo numero: "; cin >> n2;
-    while (i <= n1 && i <= n2){
-        if (n1 % i == 0 && n2 % i == 0){
-            c++;
+    int n1, n2;
+    char re;
+    do {
+        system ("cls");
+        cout << "===============================\n";
+        cout << "\t\tPESI\n";
+        cout << "===============================\n";
+        cout << "Introduce el primer numero: "; cin >> n1;
+        cout << "Introduce el segundo numero: "; cin >> n2;
+        if (n1 <= 0 || n2 <= 0){
+            cout << "Los numeros deben ser mayores a cero\n";
+            system ("pause>nul");
         }
-        i++;
-    }
-    if (c > 1){
-        cout << "Los numeros no son PESI\n";
-    } 
-    else {
-        cout << "Los numeros son PESI\n";
+        else {
+            int i = 1, c = 0;
+            while (i <= n1 && i <= n2){
+                if (n1 % i == 0 && n2 % i == 0){
+                    c++;
+                }
+                i++;
+            }
+            if (c > 1){
+                cout << "Los numeros no son PESI\n";
+            } 
+            else {
+                cout << "Los numeros son PESI\n";
+            }
+        }
+        cout << "===============================\n";
+        cout << "Desea realizar otra operacion? (s/n): "; cin >> re;
+    } while (re == 's' || re == 'S');
+    if (re == 'n' || re == 'N'){
+        cout << "===============================\n";
+        cout << "Saliendo del programa ...\n";
+        system ("pause>nul");
     }
     return 0;
 }
